d3ba57cc4b47d8204413e5ef276388e298f34d91
brunoclemente88@gmail.com <brunoclemente88@gmail.com>
2018-10-08 19:59:21 -0300

    Enviando todos os códigos da disciplina até a aula de hoje

--- P2_2018_02/src/POO/Conta.java
+++ P2_2018_02/src/POO/Conta.java
@@ -2,18 +2,138 @@ package POO;
 
 public class Conta {
 	// Atributos : o que definem O QUE É uma Conta
-	int numero;
-	String titular;
-	double saldo, limite;
-	
-	public void deposita(double valor) {
-		this.saldo += valor;
-		System.out.println("Depósito de " + valor +
-				" realizado com sucesso na conta de "
-				+ this.titular +"!");
+	private int numero;
+	private String titular;
+	private double saldo, limite;
+	private boolean status;
+
+	// 3 Construtores
+	public Conta() {
+		this.numero = 999;
+		this.titular = null;
+		this.saldo = 0;
+		this.limite = 500;
+		this.status = true;
 	}
-	
+
+	public Conta(int numero, String titular, double saldo) {
+		this.numero = numero;
+		this.titular = titular;
+		this.saldo = saldo;
+		this.limite = 500;
+		this.status = true;
+	}
+
+	public Conta(int numero, String titular, double saldo, double limite) {
+		this.numero = numero;
+		this.titular = titular;
+		this.saldo = saldo;
+		this.limite = limite;
+		this.status = true;
+	}
+
+	// Métodos:
 	public double getSaldo() {
 		return this.saldo;
 	}
+
+	public double getLimite() {
+		return this.limite;
+	}
+
+	public String getTitular() {
+		return this.titular;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		// Validar o cargo de quem pediu
+		this.numero = numero;
+	}
+
+	/*
+	 * public boolean saca(double valor) { boolean sucesso = false; // Verificando
+	 * se a Conta tem saldo //if (valor <= this.saldo + this.limite) { if
+	 * (validaSaldo(valor)) { this.saldo -= valor; System.out.println(valor +
+	 * " sacado com sucesso!"); sucesso = true; } else {
+	 * System.out.println("Não foi possível realizar o saque!"); }
+	 * 
+	 * return sucesso;
+	 * 
+	 * }
+	 */
+
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+
+	public int saca(double valor) {
+		int resultado;
+		// Verificando se a Conta tem saldo
+		if (this.status) {
+			if (valor <= this.saldo) {
+				this.saldo -= valor;
+				resultado = 1;
+			} else if (valor <= this.saldo + this.limite) {
+				this.saldo -= valor;
+				resultado = 2;
+			} else {
+				resultado = 3;
+			}
+		} else {
+			resultado = 4;
+		}
+
+		return resultado;
+
+	}
+
+	public boolean validaSaldo(double valor) {
+		boolean valido = false;
+		if (valor <= this.saldo + this.limite) {
+			valido = true;
+		}
+		return valido;
+	}
+
+	public void inverteConta() {
+		if (this.status) {
+			this.status = false;
+		} else {
+			this.status = true;
+		}
+	}
+
+	public boolean getStatus() {
+		return this.status;
+	}
+
+	public void transferePara(Conta contaDestino, double valor) {
+		int retorno = this.saca(valor);
+		// Somente faz o depósito na conta destino
+		// Se o saque na conta de origem
+		// foi bem sucedido
+		if (retorno == 1 || retorno == 2) {
+			contaDestino.deposita(valor);
+			System.out.println("Transferência realizada com sucesso");
+		} else {
+			System.out.println("Não foi possível realizar a transferencia!");
+		}
+	}
+
+	@Override
+	public String toString() {
+		String status = "";
+		if (this.status) {
+			status = "Ativa";
+		} else {
+			status = "Inativa";
+		}
+		return "Imprimindo dados da Conta do " + titular + ":\n" + "Número = " + numero + "\n" + "Saldo atual = "
+				+ saldo + "\n" + "Limite disponível = " + limite + "\n" + "(Conta " + status + ").";
+	}
+
 }
--- P2_2018_02/src/POO/Principal.java
+++ P2_2018_02/src/POO/Principal.java
@@ -4,12 +4,10 @@ import java.util.Scanner;
 
 public class Principal {
 	public static void main(String[] args) {
-		final int tam = 3;
-		Conta contaBruno = new Conta();
-		Conta contaAna = new Conta();
+		final int tam = 2;
 		
 		// Definindo os atributos do Objeto contaBruno
-		contaBruno.titular = "Bruno";
+		/*contaBruno.titular = "Bruno";
 		contaBruno.numero = 1234;
 		contaBruno.saldo = 100;
 		contaBruno.limite = 300;
@@ -18,7 +16,7 @@ public class Principal {
 		contaAna.titular = "Ana";
 		contaAna.numero = 4321;
 		contaAna.saldo = 800;
-		contaAna.limite = 400;
+		contaAna.limite = 400;*/
 		
 		// Fazendo uso dos métodos da classe Conta
 		/*System.out.println(contaBruno.getSaldo());
@@ -31,19 +29,48 @@ public class Principal {
 		// Criando vetor de Contas
 		Scanner entrada = new Scanner(System.in);
 		
-		Conta[] contas = new Conta[tam];
+		//Conta[] contas = new Conta[tam];
 		
-		for (int i = 0; i < contas.length; i++) {
+		/*for (int i = 0; i < contas.length; i++) {
 			contas[i] = new Conta();
 			System.out.println("Digite o nome do titular da conta " + (i+1));
-			contas[i].titular = entrada.nextLine();
+			//contas[i].getTitular() = entrada.nextLine();
 			System.out.println("Digite o saldo da conta " + (i+1));
-			contas[i].saldo = Integer.valueOf(entrada.nextLine());
+			contas[i].setNumero(Integer.valueOf(entrada.nextLine()));
 		}
 		
 		for (int i = 0; i < contas.length; i++) {
 			contas[i].deposita(100);
-			System.out.println(contas[i].titular + ", Saldo: R$" + contas[i].saldo);
+			System.out.println(contas[i].getTitular() +
+					", Saldo: R$" + contas[i].getSaldo());
+		}*/
+		
+		Conta[] contas = new Conta[tam];
+		
+		for (int i = 0; i < contas.length; i++) {
+			System.out.println("Digite o numero da conta");
+			int numero = Integer.valueOf(entrada.nextLine());
+			
+			System.out.println("Digite o nome do titular");
+			String nome = entrada.nextLine();
+			
+			System.out.println("Digite o saldo");
+			double saldo = Double.valueOf(entrada.nextLine());
+			
+			System.out.println("Digite o limite");
+			double limite = Double.valueOf(entrada.nextLine());
+			
+			Conta novaConta = new Conta(
+					numero, nome, saldo, limite);
+			contas[i] = novaConta;
 		}
+		
+		System.out.println(contas[0].toString());
+		System.out.println(contas[1].toString());
+		contas[0].transferePara(contas[1], 100);
+		System.out.println(contas[0].toString());
+		System.out.println(contas[1].toString());
+		
+		
 	}
 }
